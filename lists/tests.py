d5e54a000f1b92f6918bb12cf13da51449ed450f
Arthur Phillip <artphil.dev@gmail.com>
2020-09-05 20:55:35 -0300

    Implementando o Novo Projeto: Ajustando o Modelo

--- lists/tests.py
+++ lists/tests.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 from django.http import HttpRequest
 
 from lists.views import home_page
-from lists.models import Item
+from lists.models import Item, List
 
 class HomePageTest(TestCase):
 
@@ -15,16 +15,24 @@ class HomePageTest(TestCase):
 		self.client.get('/')
 		self.assertEqual(Item.objects.count(), 0)
 
-class ItemModelTest(TestCase):
+class ListAndItemModelsTest(TestCase):
 
 	def test_saving_and_retrieving_items(self):
+		my_list = List()
+		my_list.save()
+
 		first_item = Item()
 		first_item.text = 'O primeiro item'
+		first_item.list = my_list
 		first_item.save()
 
 		second_item = Item()
 		second_item.text = 'O segundo item'
+		second_item.list = my_list
 		second_item.save()
+
+		saved_list = List.objects.first()
+		self.assertEqual(saved_list, my_list)
 
 		saved_items = Item.objects.all()
 		self.assertEqual(saved_items.count(), 2)
@@ -32,7 +40,9 @@ class ItemModelTest(TestCase):
 		first_saved_item = saved_items[0]
 		second_saved_item = saved_items[1]
 		self.assertEqual(first_saved_item.text, 'O primeiro item')
+		self.assertEqual(first_saved_item.list, my_list)
 		self.assertEqual(second_saved_item.text, 'O segundo item')
+		self.assertEqual(second_saved_item.list, my_list)
 
 class ListViewTest(TestCase):
 
@@ -51,12 +61,12 @@ class ListViewTest(TestCase):
 		
 class NewListTest(TestCase):
 
-    def test_can_save_a_POST_request(self):
-        self.client.post('/lists/new', data={'item_text': 'A new list item'})
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, 'A new list item')
+	def test_can_save_a_POST_request(self):
+		self.client.post('/lists/new', data={'item_text': 'A new list item'})
+		self.assertEqual(Item.objects.count(), 1)
+		new_item = Item.objects.first()
+		self.assertEqual(new_item.text, 'A new list item')
 
-    def test_redirects_after_POST(self):
-        response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
-        self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
+	def test_redirects_after_POST(self):
+		response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
+		self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
