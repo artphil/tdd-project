b211962b8fe1ad4325554b529b905135f367f699
Arthur Phillip <artphil.dev@gmail.com>
2020-09-05 11:30:21 -0300

    Melhorando a classe de teste com setUp e tearDown

--- functional_tests.py
+++ functional_tests.py
@@ -3,36 +3,39 @@ import unittest
 
 class NewVisitorTest(unittest.TestCase):
 
-    def test_can_start_a_list_and_retrieve_it_later(self): 
-    
-        self.browser = webdriver.Firefox()
-    
-        # Maria decidiu utilizar o novo app TODO. Ela entra em sua página principal:
-        self.browser.get('http://localhost:8000')
+	def setUp(self):  
+		self.browser = webdriver.Firefox()
 
-        # Ela nota que o título da página menciona TODO
-        self.assertIn('To-Do', self.browser.title)
+	def tearDown(self):  
+		self.browser.quit()
 
-        # Ela é convidada a entrar com um item TODO imediatamente
+	def test_can_start_a_list_and_retrieve_it_later(self): 
+	
+	
+		# Maria decidiu utilizar o novo app TODO. Ela entra em sua página principal:
+		self.browser.get('http://localhost:8000')
 
-        # Ela digita "Estudar testes funcionais" em uma caixa de texto
+		# Ela nota que o título da página menciona TODO
+		self.assertIn('To-Do', self.browser.title)
 
-        # Quando ela aperta enter, a página atualiza, e mostra a lista
-        # "1: Estudar testes funcionais" como um item da lista TODO
-        
-        # Ainda existe uma caixa de texto convidando para adicionar outro item
-        # Ela digita: "Estudar testes de unidade"
+		# Ela é convidada a entrar com um item TODO imediatamente
 
-        # A página atualiza novamente, e agora mostra ambos os itens na sua lista
+		# Ela digita "Estudar testes funcionais" em uma caixa de texto
 
-        # Maria se pergunta se o site vai lembrar da sua lista. Então, ela verifica que
-        # o site gerou uma URL única para ela -- existe uma explicação sobre essa feature
+		# Quando ela aperta enter, a página atualiza, e mostra a lista
+		# "1: Estudar testes funcionais" como um item da lista TODO
+		
+		# Ainda existe uma caixa de texto convidando para adicionar outro item
+		# Ela digita: "Estudar testes de unidade"
 
-        # Ela visita a URL: a sua lista TODO ainda está armazenada
+		# A página atualiza novamente, e agora mostra ambos os itens na sua lista
 
-        # Satisfeita, ela vai dormir
-        
-        self.browser.quit()
+		# Maria se pergunta se o site vai lembrar da sua lista. Então, ela verifica que
+		# o site gerou uma URL única para ela -- existe uma explicação sobre essa feature
+
+		# Ela visita a URL: a sua lista TODO ainda está armazenada
+
+		# Satisfeita, ela vai dormir
 
 if __name__ == '__main__':
-    unittest.main()
+	unittest.main()
