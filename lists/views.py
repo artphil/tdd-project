286d6ddb51155aa78efe9a3b179ad6334823924c
Arthur Phillip <artphil.dev@gmail.com>
2020-09-05 18:05:28 -0300

    Apresentar Múltiplos Itens na Listagem

--- lists/views.py
+++ lists/views.py
@@ -3,12 +3,10 @@ from lists.models import Item
 
 # Create your views here.
 def home_page(request):
-	new_item_text = ''
 	if request.method == 'POST':
 		new_item_text = request.POST['item_text']
 		Item.objects.create(text=new_item_text)
 		return redirect('/')
-
-	return render(request, 'home.html', {
-		'new_item_text': new_item_text
-	})
+		
+	items = Item.objects.all()
+	return render(request, 'home.html', {'items': items})
